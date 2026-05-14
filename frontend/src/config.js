40a51ec60cb716a4a64ccb3c6320324a0bdcf6fe
HightD <duonghd241004@gmail.com>
2026-05-14 22:55:54 +0700

    Update Backend connection and Frontend CORS config 2

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,4 +1,4 @@
-const API_URL='https://realstate-website-5jpt.onrender.com/';
-
+// const API_URL='http://localhost:5000/';
+const API_URL = "https://realstate-website-5jpt.onrender.com/";
 //live or hosted url for backend
 export default API_URL;
