d174af6add83c9038611f8d191cf05acd7777314
HightD <duonghd241004@gmail.com>
2026-05-16 07:45:21 +0700

    update api_url frontend 2

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,6 +1,7 @@
 // const API_URL='http://localhost:5000/';
 // const API_URL = "https://realstate-website-5jpt.onrender.com/";
 
-const API_URL = import.meta.env.VITE_API_URL || "https://realstate-website-5jpt.onrender.com";
+const API_URL =
+  import.meta.env.VITE_API_URL || "https://realstate-website-5jpt.onrender.com/";
 //live or hosted url for backend
 export default API_URL;
